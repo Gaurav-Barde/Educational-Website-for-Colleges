0bedc8763f51dfd969067a93ef95ec2724ad3f29
Gaurav Barde <Gaurav-Barde@users.noreply.github.com>
2017-04-05 12:19:41 +0530

    Speed en up slider

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -38,7 +38,7 @@
   //function for auto slide show
   function slideNext () {
 
-    $carouInner.animate({'left': '-200%'}, settings.duration, function() {  //moving the slider wrapper to the right by
+    $carouInner.animate({'left': '-200%'}, settings.duration - 200, function() {  //moving the slider wrapper to the right by
 
       $carouInner.css('left', '-100%');
 
@@ -52,7 +52,7 @@
     
     clearInterval(sliderInterval);
 
-    $carouInner.animate({'left': '0%'}, settings.duration, function() {
+    $carouInner.animate({'left': '0%'}, settings.duration - 200, function() {
 
       $carouInner.css('left', '-100%');
 
